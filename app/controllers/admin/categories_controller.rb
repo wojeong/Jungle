7a65143cae5ec70c11087e9e0252eed1bf5c2486
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-03-16 00:36:22 -0400

    Error at index.html.erb calling categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   #http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
-    @categories = Category.all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
