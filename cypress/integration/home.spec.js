971f38ced9b320fa7cffa33bcac6b19e70150a59
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-03-16 22:41:05 -0400

    Cypress Test Home Page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('HomePage', () => {
+  beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+      cy.get(".products article").should("be.visible");
+    });
+
+    it("There are 12 products on the page", () => {
+      cy.get(".products article").should("have.length", 12);
+    });
+})
